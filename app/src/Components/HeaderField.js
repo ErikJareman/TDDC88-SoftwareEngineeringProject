104f774cc57fb09f5226478bbff369999cd6e9f9
Philip Nylén <phiny516@student.liu.se>
2021-10-06 17:07:04 +0000

    Home page structure

---
+++ app/src/Components/HeaderField.js
@@ -0,0 +1,19 @@
+/**
+ * HeaderField.js
+ *
+ * Top header which is constant across all pages
+ *
+ * function HeaderField() - Philip Nylén - FINAL
+ */
+
+import headerLogo from "../assets/headerLogo.png";
+import "./HeaderField.css";
+
+export default function HeaderField() {
+  return (
+    <div class="container-fluid" id="headerArea">
+        <img src={headerLogo} className="header-logo" alt="Not found" />
+        <h1 id="headerText"> Norrköping</h1>
+    </div>
+  );
+}
--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
+import PatientNavList from "../../Components/PatientNavList";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div>
       <h1>This is the home page!</h1>
+      <PatientNavList />
     </div>
   );
 }
