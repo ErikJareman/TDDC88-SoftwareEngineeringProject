6960ec5875fbb07015f7d6a61fc9199a0da79780
Gregor ster <greos257@student.liu.se>
2021-11-02 17:50:47 +0100

    Updated tests and testing config

--- app/testing/regression_testing/1_regTestHomePage.test.js
+++ app/testing/regression_testing/1_regTestHomePage.test.js
@@ -4,7 +4,7 @@ const { testPatientList } = require('./1_test_home/1_testPatientList.js')
 const { getHomePageHeading } = require('./1_test_home/homeUtilities.js')
 const { goToPatient } = require('./3_test_patient/patientUtilities.js')
 
-describe('Testing document functionality', () => {
+describe('Testing home page', () => {
   beforeAll(async () => {
     driver = await buildDriver(url)
     // await isLoggedIn(driver)
@@ -15,15 +15,15 @@ describe('Testing document functionality', () => {
     return await driver.quit()
   })
 
-  test('Home-page heading', async () => {
+  test('Test home-page heading', async () => {
     expect(await getHomePageHeading(driver)).toBe('Norrköping')
   })
 
-  test('Test patient info', async () => {
-    expect(await goToPatient(driver)).toBe(true)
+  test('Test select patient in patient list', async () => {
+    expect(await goToPatient(driver, 'Fredrik Olsson')).toBe(true)
   })
 
-  test('Test patient list in home view', async () => {
+  test('Test content in patient list in home view', async () => {
     expect(await testPatientList(driver)).toBe(2)
   })
 
--- app/testing/regression_testing/2_regTestPatientPage.test.js
+++ app/testing/regression_testing/2_regTestPatientPage.test.js
@@ -2,7 +2,7 @@ const { url } = require('./testConfig.js')
 const { buildDriver } = require('./testUtilities.js')
 const { testPatientInfoBox } = require('./3_test_patient/1_testPatientInfoBox.js')
 
-describe('Testing document functionality', () => {
+describe('Testing pateint page', () => {
   beforeAll(async () => {
     driver = await buildDriver(url)
     // await isLoggedIn(driver)
@@ -13,7 +13,7 @@ describe('Testing document functionality', () => {
     return await driver.quit()
   })
 
-  test('Home-page heading', async () => {
+  test('Test patient info for correct data', async () => {
     expect(await testPatientInfoBox(driver)).toBe(true)
   })
 
