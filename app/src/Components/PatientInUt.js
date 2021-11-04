8e7460069f5d8d4b49b6b99afbef2544159d02b4
Viktor Storsved <vikst890@student.liu.se>
2021-11-04 13:51:01 +0100

    Add comments in code, change class names

--- app/src/Components/PatientInUt.js
+++ app/src/Components/PatientInUt.js
@@ -1,21 +1,15 @@
-/**
- * PatientNavList.js
- *
- * function PatientNavList() - Philip Nylén, Erik Jareman - DRAFT
- */
 import './PatientInUt.css'
-//  import { Link } from 'react-router-dom'
-//  import trends from '../assets/trends.png'
-//  import notificationBell from '../assets/notificationBell.png'
 import React from 'react'
 
 /**
- * The function PatientNavList renders the list of patients, currently
+ * The function PatientInUt renders the list of patients intravenous injections, currently
  * only psuedo-data
- * Philip Nylén, Erik Jareman
- * FIXING
+ * Viktor Storsved, Marcus Vidgren
  */
 export default function PatientInUt () {
+  /*
+  Dummy data, shall be updated when the back-end is in place
+  */
   const intravenousInjection = [
     {
       id: '1',
@@ -33,14 +27,14 @@ export default function PatientInUt () {
   ]
 
   return (
-    <ul className ="inUt">
+    <ul className ="injections">
       {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
       <li>
       <table>
-      <h3 className='table-long'> Typ</h3>
-      <h3 className='table-long'>Storlek</h3>
-      <h3 className='table-long'>Lokalisation</h3>
-      <h3 className='table-long'>Tid</h3>
+      <h3 className='inUtTextBox'> Typ</h3>
+      <h3 className ='inUtTextBox'>Storlek</h3>
+      <h3 className='inUtTextBox'>Lokalisation</h3>
+      <h3 className='inUtTextBox'>Tid</h3>
       </table>
       </li>
       {intravenousInjection.map((injection) => {
@@ -48,10 +42,10 @@ export default function PatientInUt () {
           <li key={injection.id}>
             <table>
               {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
-              <h3 className='table-long'>{injection.type}</h3>
-              <h3 className='table-long'>{injection.size}</h3>
-              <h3 className='table-long'>{injection.locatization}</h3>
-              <h3 className='table-long'>{injection.time}</h3>
+              <h3 className='inUtTextBox'>{injection.type}</h3>
+              <h3 className='inUtTextBox'>{injection.size}</h3>
+              <h3 className='inUtTextBox'>{injection.locatization}</h3>
+              <h3 className='inUtTextBox'>{injection.time}</h3>
             </table>
           </li>
         )
