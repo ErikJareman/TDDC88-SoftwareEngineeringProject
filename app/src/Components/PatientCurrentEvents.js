f16d12e2cabbb6d930fce2ef50642e81a220822d
Martin Jonson <marjo886@student.liu.se>
2021-11-04 13:33:57 +0100

    Different images

--- app/src/Components/PatientCurrentEvents.js
+++ app/src/Components/PatientCurrentEvents.js
@@ -11,10 +11,10 @@ import EventCard from './EventCard'
 export default function PatientCurrentEvents () {
   return (
     <div>
-      <EventCard name='Medicin ordination' time='17.00' color='blue' />
-      <EventCard name='Smörgås och saft' time='15.40' color='green' />
-      <EventCard name='Medicin dosering' time='13.38' color='green' />
-      <EventCard name='Patient inlagd' time='13.00' color='blue' />
+      <EventCard name='Medicin ordination' time='17.00' color='blue' image='portal' />
+      <EventCard name='Smörgås och saft' time='15.40' color='green' image='firstAid' />
+      <EventCard name='Medicin dosering' time='13.38' color='green' image='accessability' />
+      <EventCard name='Patient inlagd' time='13.00' color='blue' image='firstAid' />
     </div>
   )
 }
