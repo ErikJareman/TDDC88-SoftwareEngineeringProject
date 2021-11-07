44d5a9c0ad6ebc30cb8e35d7a571abc9ca63868d
Philip Nylén <phiny516@student.liu.se>
2021-11-07 15:48:29 +0100

    first iteration DB done, connection established between front and back

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,28 @@
+# backend.py
+#
+# This is the server that talks with front-end and database
+#
+# Isak Berntsson, Hugo Hallstensson Riddargård, Philip Nylén
+#
+# FIXING
+
 from make_data import make_data
 from flask import Flask, jsonify
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 make_data(50)
 
+
 @app.route('/patients/<string:location>')
 def patients_at(location):
-    df_patients = pd.read_csv("mock_patient_data.csv")
-    return jsonify(df_patients[df_patients["Location"] == location].to_dict())
+    df_patients = pd.read_csv("mock_patient_data.csv", delimiter=',')
+    patients = df_patients[df_patients["location"] == location]
+    return jsonify(patients.to_dict('records'))
+    
 
 @app.route("/")
 def hello_world():
--- backend/make_data.py
+++ backend/make_data.py
@@ -1,9 +1,14 @@
+# make_data.py
+#
+# generates fake data that gets stored in mock_patient_data.csv
+#
+# Isak Berntsson, Hugo Hallstensson Riddargård, Philip Nylén
+#
+# FIXING
+
 import pandas as pd
 from faker import Faker
 import random
-
-
-
 
 def generate_SSN():
     y = str(random.randint(1920,2020))
@@ -12,23 +17,48 @@ def generate_SSN():
     d = str(random.randint(1,28))
     d = "0"+d if int(d)<10 else d
     x = str(random.randint(1000,9999))
+
+    return y+m+d+"-"+x
+
+def generate_timer():
+    min = str(random.randint(0,10))
+    min = "0"+min if int(min)<10 else min
+    sek = str(random.randint(0,59))
+    sek = "0"+sek if int(sek)<10 else sek
     
-    return y+m+d+"-"+x    
+    return min+":"+sek
+
+def generate_arrival():
+    tim = str(random.randint(0,23))
+    tim = "0"+tim if int(tim)<10 else tim
+    min = str(random.randint(0,59))
+    sek = "0"+min if int(min)<10 else min
+    
+    return tim+":"+min
+
+def generate_reason():
+    reasons = ["Benbrott", "Buksmartor", "Hjartkramp", "Ryggvark"]
+    return random.choice(reasons)
+        
 def make_data(NUM_PATIENTS = 100):
     LOCATIONS = ["Linkoping", "Norrkoping", "Motala"]
-    df_patients = pd.DataFrame(columns = ["Name", "SSN","Location", "Team", "Room"])
+    df_patients = pd.DataFrame(columns = ["id", "timer", "arrival", "reason", "name", "SSN","location", "team", "room"])
 
     fake = Faker()
-    df_patients["Name"] = [fake.name() for _ in range(NUM_PATIENTS)]
-    df_patients["Location"] = [random.choice(LOCATIONS) for _ in range(NUM_PATIENTS)]
+    df_patients["id"] = [i for i in range(NUM_PATIENTS)]
+    df_patients["timer"] = [generate_timer() for _ in range(NUM_PATIENTS)]
+    df_patients["arrival"] = [generate_arrival() for _ in range(NUM_PATIENTS)]
+    df_patients["reason"] = [generate_reason() for _ in range(NUM_PATIENTS)]
+    df_patients["name"] = [fake.name() for _ in range(NUM_PATIENTS)]
+    df_patients["location"] = [random.choice(LOCATIONS) for _ in range(NUM_PATIENTS)]
     df_patients["SSN"] = [generate_SSN() for _ in range(NUM_PATIENTS)]
-    df_patients["Team"] = [random.choice(list("ABC")) for _ in range(NUM_PATIENTS)]
-    df_patients["Room"] = [random.randint(1, 10) for _ in range(NUM_PATIENTS)]
+    df_patients["team"] = [random.choice(list("ABC")) for _ in range(NUM_PATIENTS)]
+    df_patients["room"] = [random.randint(1, 10) for _ in range(NUM_PATIENTS)]
 
     print(df_patients)
 
     with open("mock_patient_data.csv", "w+") as f:
-        df_patients.to_csv(f)
+        df_patients.to_csv(f, index=False)
     return
         
 if __name__ == '__main__':
