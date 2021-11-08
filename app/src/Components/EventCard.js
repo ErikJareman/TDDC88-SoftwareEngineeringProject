9309c1f5f4004d4604f9dba24f370fd9fb95b8cf
Martin Jonson <marjo39@liu.se>
2021-11-08 15:43:17 +0100

    Cleaned up code, made icons clickable

--- app/src/Components/EventCard.js
+++ app/src/Components/EventCard.js
@@ -1,30 +1,31 @@
 import './EventCard.css'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import homeImage from '../assets/home.png'
 import portalImage from '../assets/portal.png'
 import firstAidImage from '../assets/first_aid.png'
 import accessabilityImage from '../assets/accessability.png'
 
-class EventCard extends Component {
-  render () {
-    const imgMap = new Map()
-    imgMap.set('home', homeImage)
-    imgMap.set('portal', portalImage)
-    imgMap.set('firstAid', firstAidImage)
-    imgMap.set('accessability', accessabilityImage)
-    return (
-      <>
-        <table id='patient-inlagd-group'>
-          <tr>
-            <th><div style={{ backgroundColor: this.props.color }} id='rectangle'></div></th>
-            <th><h4>{this.props.name}</h4></th>
-            <th><img src={imgMap.get(this.props.image)} id='shape'></img></th>
-            <th><h4>{this.props.time}</h4></th>
-          </tr>
-        </table>
-      </>
-    )
-  }
+function EventCard (props) {
+  const [color, setColor] = useState(props.color)
+  const imgMap = new Map()
+  imgMap.set('home', homeImage)
+  imgMap.set('portal', portalImage)
+  imgMap.set('firstAid', firstAidImage)
+  imgMap.set('accessability', accessabilityImage)
+  return (
+    <>
+      <table
+        id='patient-inlagd-group'
+        onClick={() => setColor('grey')}>
+        <tr>
+          <th><div style={{ backgroundColor: color }} id='rectangle'></div></th>
+          <th><h4>{props.name}</h4></th>
+          <th><img src={imgMap.get(props.image)} id='shape'></img></th>
+          <th><h4>{props.time}</h4></th>
+        </tr>
+      </table>
+    </>
+  )
 }
 
 export default EventCard
