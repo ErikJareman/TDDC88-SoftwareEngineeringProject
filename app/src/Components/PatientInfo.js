c9b5dfb6a67cedf098cac3b093b15c981c382373
nikku485 <nikku485@liu.se>
2021-11-09 21:41:00 +0100

    minor fixes with the data to be displayed

--- app/src/Components/PatientInfo.js
+++ app/src/Components/PatientInfo.js
@@ -17,7 +17,7 @@ export default function PatientInfo (patient) {
   return (
     <div className='PatientInfo-div'>
       <h1> Patientinfo  </h1>
-      <h5><Icon name='user' />{patient.patient.name}, {patient.patient.pnum}</h5>
+      <h5><Icon name='user' />{patient.patient.name}, {patient.patient.SSN}</h5>
       <h5><Icon name='address book' /> ISS: </h5>
       <h5><Icon name='ambulance' /> Ambulans </h5>
       <TeamView />
