6f286a7b29415afc9ddd3f514dd67178d1f7596d
unknown <philo561@student.liu.se>
2021-11-12 09:38:10 +0100

    test för vitals route

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,24 @@ def patients_at(location):
     df_patients = pd.read_csv("mock_patient_data.csv", delimiter=',')
     patients = df_patients[df_patients["location"] == location]
     return jsonify(patients.to_dict('records'))
-    
+
+@app.route('/patients/<integer:patient_id>/vitals')
+def patient_vitals(patient_id):
+    df_vitals = pd.read_csv("mock_vitals.csv", delimiter=',')
+    vitals = df_vitals[df_vitals["id"] == patient_id]
+    return jsonify(vitals.to_dict('records'))
+
+""" @app.route('/patients/<int:patient_id>/injections')
+def patient_vitals(patient_id):
+    df_injections = pd.read_csv("mock_injections.csv", delimiter=',')
+    injectiosn = df_injections[df_injections["id"] == patient_id]
+    return jsonify(df_injections.to_dict('records'))
+
+@app.route('/patients/<int:patient_id>/events')
+def patient_events(patient_id):
+    df_events = pd.read_csv("mock_events.csv", delimiter=',')
+    events = df_events[df_events["id"] == patient_id]
+    return jsonify(events.to_dict('records')) """
 
 @app.route("/")
 def hello_world():
