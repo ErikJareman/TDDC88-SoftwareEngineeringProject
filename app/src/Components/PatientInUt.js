b7d02df6ba8dc5c17d414fa6417e895ee0e976ec
Viktor Storsved <vikst890@student.liu.se>
2021-11-16 19:20:15 +0100

    Show waring text if there is no in - ut injections

--- app/src/Components/PatientInUt.js
+++ app/src/Components/PatientInUt.js
@@ -10,22 +10,28 @@ export default function PatientInUt (patient) {
 /**
  * Constant to store injections in a way so .map function works
  */
+  let empty = true
   const [injections, setInjections] = useState([])
-
-  console.log(patient.patient)
   useEffect(() => {
     axios.get('http://localhost:5000/patients/' + patient.patient + '/injections')
       .then(res => {
         const patientInjection = res.data
+        console.log('res data')
         console.log(res.data)
-        //  obj.localization = injections
-        console.log('Injections')
-        console.log(patientInjection)
-        console.log('array')
+        console.log(res.data)
         setInjections(patientInjection)
+        if (injections.length > 0) {
+          console.log('Ej tomt')
+          empty = false
+          console.log(empty)
+        }
       })
   }, [])
-  return (
+  /**
+    * This if-statement checks if there is any data to show in in - ut farter
+    */
+  if (Object.entries(injections).length > 0) {
+    return (
     <ul>
       {/* <li>
       <table>
@@ -37,7 +43,9 @@ export default function PatientInUt (patient) {
       </table>
       </li> */}
       {injections.map((injection) => {
-        return (
+        console.log(injections.length)
+        if (injections.length > 0) {
+          return (
           <li key={injection.id}>
             <div id="linkList">
                 {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
@@ -49,8 +57,34 @@ export default function PatientInUt (patient) {
                 <h3 className='inUtTextBox'>{injection.timeout.split('.', 1)}</h3>
             </div>
           </li>
-        )
+          )
+        } else {
+          console.log(injections.length)
+          return (
+            <li key={injection.id}>
+              <div id="linkList">
+          <h3 className='inUtTextBox'>No data available</h3>
+          </div>
+          </li>
+          )
+        }
       })}
     </ul>
-  )
+    )
+  } else {
+    /**
+     * This if-statement checks if there is any data to show in in - ut farter
+     */
+    console.log('det är tomt')
+    return (
+      <ul>
+      {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
+      <li>
+      <table>
+      <h3 className='inUtTextBox'> Ingen data tillgängligt </h3>
+      </table>
+      </li>
+      </ul>
+    )
+  }
 }
