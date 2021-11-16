e99e5c106f286384a2d9d1c854576f5f939b2939
gusch593 <gusch593@student.liu.se>
2021-11-16 12:14:44 +0100

    Added OverviewNavBar for home page + Changed color home page + some small allignment fixes in PatientNavList.css - Gustav and John

---
+++ app/src/Components/OverviewNavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './OverviewNavBar.css'
+
+export default function OverviewNavBar () {
+  return (
+    <div id="overviewArea">
+         <h1 id="overviewText"> Overview </h1>
+    </div>
+  )
+}
