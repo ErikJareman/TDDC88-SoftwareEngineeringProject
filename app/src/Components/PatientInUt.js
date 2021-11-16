569b34676be08957634bf11d1eee861aecf87d85
Viktor Storsved <vikst890@student.liu.se>
2021-11-16 16:43:12 +0100

    Clean up code

--- app/src/Components/PatientInUt.js
+++ app/src/Components/PatientInUt.js
@@ -3,18 +3,13 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 /**
- * The function PatientInUt renders the list of patients intravenous injections, currently
- * only psuedo-data
+ * The function PatientInUt renders the list of patients intravenous injections
  * Viktor Storsved, Marcus Vidgren
  */
 export default function PatientInUt (patient) {
-  /*
-  Dummy data, shall be updated when the back-end is in place
-  */
-  //  const inUt = []
-  //  const obj = { localization: 'bar', procedure: 42, timein: 0, timeout: 0, type: 'aa', value: 0 }
-  //  const myArray = []
-  //  var injections = []
+/**
+ * Constant to store injections in a way so .map function works
+ */
   const [injections, setInjections] = useState([])
 
   console.log(patient.patient)
@@ -58,30 +53,4 @@ export default function PatientInUt (patient) {
       })}
     </ul>
   )
-
-  // return (
-  //   <ul className ="injections">
-  //     {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
-  //     <li>
-  //     <table>
-  //     <h3 className='inUtTextBox'> {injections.type}</h3>
-  //     <h3 className ='inUtTextBox'>Storlek</h3>
-  //     <h3 className='inUtTextBox'>Lokalisation</h3>
-  //     <h3 className='inUtTextBox'>Tid</h3>
-  //     </table>
-  //     </li>
-  //     {injections.map((injection) => {
-  //       return (
-  //         <li key={injection.id}>
-  //           <table>
-  //             {/* Children in order <table> --> <thead> --> <tr> --> <td> to avoid warning, not <table> --> <h3> */}
-  //             <h3 className='inUtTextBox'>{injection.type}</h3>
-  //             <h3 className='inUtTextBox'>{injection.size}</h3>
-  //             <h3 className='inUtTextBox'>{injection.locatization}</h3>
-  //             <h3 className='inUtTextBox'>{injection.time}</h3>
-  //           </table>
-  //         </li>
-  //       )
-  //     })}
-  //   </ul>)
 }
