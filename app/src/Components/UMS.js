c3e56e04c939fc2d9a2144506d8d07ba564a7ca5
Philip Nylén <phiny516@student.liu.se>
2021-11-17 20:00:01 +0100

    one solution to the ums error

--- app/src/Components/UMS.js
+++ app/src/Components/UMS.js
@@ -16,52 +16,67 @@ import axios from 'axios'
  */
 
 export default function UMS (props) {
+  console.log('1. Patient id: ' + props.patient.patient.id)
+  const [isLoading, setLoading] = useState(true)
   const [UMS, setUMS] = useState([])
 
   useEffect(() => {
+    console.log('2. Data sent in: ' + props.patient.patient.id)
     axios.get('https://backend-c4company.herokuapp.com/patients/' + props.patient.patient.id + '/ums')
       .then(res => {
         const temp = res.data
+        console.log('3. THIS IS WHAT RENDERS: ' + temp[0].sensLevel)
         setUMS(temp)
+        setLoading(false)
       })
-  }, setUMS)
+  }, [])
 
   const modifyUMS = () => {
-    if (UMS[0].sensLevel === 1) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
-    } else if (UMS[0].sensLevel === 2) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-1').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-2').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-3').setAttributeNS(null, 'fill', 'red')
-    } else if (UMS[0].sensLevel === 3) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-1').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-2').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens2-3').setAttributeNS(null, 'fill', 'red')
-      svgObject.getElementById('oversens3').setAttributeNS(null, 'fill', 'red')
-    }
-    if (UMS[0].medCondition) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('medCondition').setAttributeNS(null, 'fill', 'red')
-    }
-    if (UMS[0].careDeviation) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('careDeviation').setAttributeNS(null, 'fill', 'blue')
-    }
-    if (UMS[0].infection) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('infection').setAttributeNS(null, 'fill', 'yellow')
-    }
-    if (UMS[0].noStructureInfo) {
-      const svgObject = document.getElementById('svg-object').contentDocument
-      svgObject.getElementById('noStructureInfo').setAttributeNS(null, 'fill', 'red')
+    console.log('4. Patient sensitivity: ')
+    if (UMS[0] !== undefined) {
+      console.log('5. ' + UMS[0].sensLevel)
+      if (UMS[0].sensLevel === 1) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
+      } else if (UMS[0].sensLevel === 2) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-1').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-2').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-3').setAttributeNS(null, 'fill', 'red')
+      } else if (UMS[0].sensLevel === 3) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('oversens1').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-1').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-2').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens2-3').setAttributeNS(null, 'fill', 'red')
+        svgObject.getElementById('oversens3').setAttributeNS(null, 'fill', 'red')
+      }
+      if (UMS[0].medCondition) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('medCondition').setAttributeNS(null, 'fill', 'red')
+      }
+      if (UMS[0].careDeviation) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('careDeviation').setAttributeNS(null, 'fill', 'blue')
+      }
+      if (UMS[0].infection) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('infection').setAttributeNS(null, 'fill', 'yellow')
+      }
+      if (UMS[0].noStructureInfo) {
+        const svgObject = document.getElementById('svg-object').contentDocument
+        svgObject.getElementById('noStructureInfo').setAttributeNS(null, 'fill', 'red')
+      }
     }
   }
   return (
-        <object className='UMS-logo' onLoad={modifyUMS} id="svg-object" data={umsLogo} type="image/svg+xml"></object>
+    <div>
+    {
+      isLoading === true
+        ? <object className='UMS-logo' />
+        : <object className='UMS-logo' onLoad={modifyUMS} id="svg-object" data={umsLogo} type="image/svg+xml"></object>
+    }
+    </div>
   )
 }
