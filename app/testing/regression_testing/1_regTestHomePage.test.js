83a5051879df68c52bc9ca5219863ed66d74ab72
Gregor ster <greos257@student.liu.se>
2021-11-18 00:22:47 +0100

    deleted faulty test

--- app/testing/regression_testing/1_regTestHomePage.test.js
+++ app/testing/regression_testing/1_regTestHomePage.test.js
@@ -1,6 +1,5 @@
 const { url } = require('./testConfig.js')
 const { buildDriver, sleep } = require('./testUtilities.js')
-const { getPatientListLength } = require('./1_test_home/1_testPatientList.js')
 const { getHomePageHeading } = require('./1_test_home/homeUtilities.js')
 const { goToPatient } = require('./3_test_patient/patientUtilities.js')
 const { loginIfNeeded } = require('./2_test_login/loginUtilities.js')
@@ -30,13 +29,5 @@ describe('Testing home page', () => {
 
   test('#1.2 (R2) : Test select patient in patient list', async () => {
     expect(await goToPatient(driver, 'Benbrott')).toBe(true)
-  })
-
-  test('#1.3 (SRS3.1.1.2) : Test patient list length in home view', async () => {
-    await driver.get(url + 'home')
-    await sleep(500)
-    const patientCount = await getPatientListLength(driver)
-    const overTen = patientCount > 10
-    expect(overTen).toBe(true)
   })
 })
