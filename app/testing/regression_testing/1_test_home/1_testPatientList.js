99eb96bee2167476cc29146d7ee40a2ff1d24f90
Gregor ster <greos257@student.liu.se>
2021-11-18 15:03:53 +0100

    added new tests for U50 and U12

--- app/testing/regression_testing/1_test_home/1_testPatientList.js
+++ app/testing/regression_testing/1_test_home/1_testPatientList.js
@@ -1,6 +1,4 @@
 const { By, until } = require('selenium-webdriver')
-const { getElementText, findElementInList } = require('../testUtilities.js')
-const { getPatientList } = require('./homeUtilities.js')
 
 /** Looks for the patient list in home page and returns the amount of
  * patients in the list */
@@ -10,34 +8,4 @@ async function getPatientListLength (driver) {
   return patientList.length
 }
 
-// REASON FOR VISIT IS HARD TO TEST WITHRANDOMLY GENERATED DATA THAT DIFFERS BETWEEN TEST-OCCASIONS
-
-/** Looks for patients in patient list on home page and checks if the
- * reason for visit is diplayed with the correct patient */
-async function getReasonForVisit (driver) {
-  const result = {
-    patient_1_Ok: false,
-    patient_2_Ok: false
-  }
-  const patientList = await getPatientList(driver)
-  console.log(patientList.length)
-  for (let i = 0; i < patientList.length; i++) {
-    const elementText = await patientList[i].getText().then((val) => {
-      return val
-    })
-    console.log(elementText)
-  }
-  const testPatient1 = await getElementText(
-    await findElementInList(patientList, 'Timothy Stone'))
-  const testPatient2 = await getElementText(
-    await findElementInList(patientList, 'Judy Rice'))
-  if (testPatient1.includes('Benbrott')) {
-    result.patient_1_Ok = true
-  }
-  if (testPatient2.includes('Buksmartor')) {
-    result.patient_2_Ok = true
-  }
-  return result
-}
-
-module.exports = { getPatientListLength, getReasonForVisit }
+module.exports = { getPatientListLength }
