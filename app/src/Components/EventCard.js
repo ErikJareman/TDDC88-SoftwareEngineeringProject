cfa54cf1157f9347262150d1020055fad6007b51
Lukas Lagerfors <lukla431@student.liu.se>
2021-11-18 11:13:38 +0100

    Working & progress

--- app/src/Components/EventCard.js
+++ app/src/Components/EventCard.js
@@ -1,31 +1,24 @@
 import './EventCard.css'
-import React, { useState } from 'react'
+import React from 'react'
 import homeImage from '../assets/home.png'
 import portalImage from '../assets/portal.png'
 import firstAidImage from '../assets/first_aid.png'
 import accessabilityImage from '../assets/accessability.png'
 
 function EventCard (props) {
-  const [color, setColor] = useState(props.color)
   const imgMap = new Map()
   imgMap.set('home', homeImage)
   imgMap.set('portal', portalImage)
   imgMap.set('firstAid', firstAidImage)
   imgMap.set('accessability', accessabilityImage)
   return (
-    <>
-      <table
-        id='patient-inlagd-group'
-        onClick={() => setColor('grey')}>
+      <table id='patient-inlagd-group'>
         <tr>
-          <th><div style={{ backgroundColor: color }} id='rectangle'></div></th>
-          <th><h4>{props.name}</h4></th>
-          <th><img src={imgMap.get(props.image)} id='shape'></img></th>
-          <th><h4>{props.time}</h4></th>
+         <th><img src={imgMap.get(props.image)} id='shape'></img></th>
+         <th><h4>{props.name}</h4></th>
+         <th><h4>{props.time}</h4></th>
         </tr>
       </table>
-    </>
   )
 }
-
 export default EventCard
