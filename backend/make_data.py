40b7d33097f7934130e4db65293f0a4366f36c4a
Martin Jonson <marjo39@liu.se>
2021-11-23 16:04:03 +0100

    python code standard

--- backend/make_data.py
+++ backend/make_data.py
@@ -1,10 +1,10 @@
-# make_data.py
-#
-# generates fake data that gets stored in mock_patient_data.csv
-#
-# Isak Berntsson, Hugo Hallstensson Riddargård, Philip Nylén
-#
-# FIXING
+'''make_data.py
+
+generates fake data that gets stored in mock_patient_data.csv
+
+Isak Berntsson, Hugo Hallstensson Riddargård, Philip Nylén
+
+FIXING '''
 
 import pandas as pd
 from faker import Faker
@@ -14,201 +14,216 @@ import datetime
 from helper_funcs import random_times
 
 
-def generate_SSN():
-    y = str(random.randint(1920,2020))
-    m = str(random.randint(1,12))
-    m = "0"+m if int(m)<10 else m
-    d = str(random.randint(1,28))
-    d = "0"+d if int(d)<10 else d
-    x = str(random.randint(1000,9999))
+def generate_ssn():
+    year = str(random.randint(1920, 2020))
+    month = str(random.randint(1, 12))
+    month = "0"+month if int(month) < 10 else month
+    day = str(random.randint(1, 28))
+    day = "0"+day if int(day) < 10 else day
+    last = str(random.randint(1000, 9999))
 
-    return y+m+d+"-"+x
+    return year + month + day + "-" + last
 
 def generate_arrival():
-    tim = str(random.randint(0,23))
-    tim = "0"+tim if int(tim)<10 else tim
-    min = str(random.randint(0,59))
-    sek = "0"+min if int(min)<10 else min
-    
-    return tim+":"+min
+    tim = str(random.randint(0, 23))
+    tim = "0"+tim if int(tim) < 10 else tim
+    minimum = str(random.randint(0, 59))
+    sek = "0"+minimum if int(minimum) < 10 else minimum
+
+    return tim + ":" + sek
 
 def generate_reason():
-    reasons = ["Benbrott", "Buksmartor", "Hjartkramp", "Ryggvark"]
-    return random.choice(reasons)
+    REASONS = ["Benbrott", "Buksmartor", "Hjartkramp", "Ryggvark"]
+    return random.choice(REASONS)
 
-
-     
-
-def make_data(NUM_PATIENTS = 100):
+def make_data(num_patients=100):
     LOCATIONS = ["Linkoping", "Norrkoping", "Motala"]
-    df_patients = pd.DataFrame(columns = ["id", "triageLevel", "arrival", "reason", "name", "SSN","location", "team", "room"])
-    df_vitals = pd.DataFrame(columns = ["id", "time", "type", "value"])
-    df_injections = pd.DataFrame(columns = ["id", "timein", "timeout", "type", "value", "localization", "procedure"])
-    df_events = pd.DataFrame(columns = ["id", "time", "category", "type", "sent"])
-    df_ums = pd.DataFrame(columns= ["id", "sensLevel", "medCondition", "careDeviation", "infection", "noStructureInfo"])
+    df_patients = pd.DataFrame(columns=["id", "triageLevel", "arrival",
+                                        "reason", "name", "SSN", "location",
+                                        "team", "room"])
+    df_vitals = pd.DataFrame(columns=["id", "time", "type", "value"])
+    df_injections = pd.DataFrame(columns=["id", "timein", "timeout",
+                                          "type", "value", "localization",
+                                          "procedure"])
+    df_events = pd.DataFrame(columns=["id", "time", "category", "type", "sent"])
+    df_ums = pd.DataFrame(columns=["id", "sens_level", "med_condition",
+                                   "care_deviation", "infection", "no_structure_info"])
 
     fake = Faker()
-    df_patients["id"] = [i for i in range(NUM_PATIENTS)]
-    df_patients["triageLevel"] = [random.randint(1, 4) for _ in range(NUM_PATIENTS)]
-    df_patients["arrival"] = random_times(NUM_PATIENTS,24*60)
-    df_patients["reason"] = [generate_reason() for _ in range(NUM_PATIENTS)]
-    df_patients["name"] = [fake.name() for _ in range(NUM_PATIENTS)]
-    df_patients["location"] = [random.choice(LOCATIONS) for _ in range(NUM_PATIENTS)]
-    df_patients["SSN"] = [generate_SSN() for _ in range(NUM_PATIENTS)]
-    df_patients["team"] = [random.choice(list("123")) for _ in range(NUM_PATIENTS)]
-    df_patients["room"] = [random.randint(1, 10) for _ in range(NUM_PATIENTS)]
-    
-    df_patients.sort_values(by=["team","id"],inplace=True)
-  
-  
-  
-    """
-    filling in vitals. 
-    base values for each patient is normally distributed with  mean of standard values.
-    
-    new vital values can be generated using functions in helper_funs.py
-    
-    """
-    
-    blood_pressures = [(90,60),(95, 75), (84,62), (78,59)]
+    df_patients["id"] = [range(num_patients)]
+    df_patients["triageLevel"] = [random.randint(1, 4) for _ in range(num_patients)]
+    df_patients["arrival"] = random_times(num_patients, 24 * 60)
+    df_patients["reason"] = [generate_reason() for _ in range(num_patients)]
+    df_patients["name"] = [fake.name() for _ in range(num_patients)]
+    df_patients["location"] = [random.choice(LOCATIONS) for _ in range(num_patients)]
+    df_patients["SSN"] = [generate_ssn() for _ in range(num_patients)]
+    df_patients["team"] = [random.choice(list("123")) for _ in range(num_patients)]
+    df_patients["room"] = [random.randint(1, 10) for _ in range(num_patients)]
+
+    df_patients.sort_values(by=["team", "id"], inplace=True)
+
+    # filling in vitals. base values for each patient is normally distributed with mean of
+    # standard values. new vital values can be generated using functions in helper_funs.py
+
+    BLOOD_PRESSURES = [(90, 60), (95, 75), (84, 62), (78, 59)]
     vitals_counter = 0
-    times = random_times(4 ,2*60)
-    
-    for id in df_patients.id:
-        
-        for t in times:
-            
-            df_vitals.loc[vitals_counter,:] = [id, t,"Puls", round(75*np.random.normal(1,.15))]
-            vitals_counter +=1
-            
-            df_vitals.loc[vitals_counter,:] = [id, t, "Kroppstemperatur", round(37.2+np.random.normal(0,.1), 1)]
-            vitals_counter +=1
-            
-            
-            df_vitals.loc[vitals_counter,:] = [id, t, "Blodtryck", (blood_pressures[np.random.randint(0,len(blood_pressures))] )]
-            vitals_counter +=1
-            
-            df_vitals.loc[vitals_counter,:] = [id, t, "Andningsfrekvens", round(19*np.random.normal(1,.3),1)]
-            vitals_counter +=1
+    times = random_times(4, 2 * 60)
 
+    for pid in df_patients.id:
 
-    """[Filling in df_injectios. Will need to add all available types, localizationsm and procedures.
-        
-    ]
-    """
-    INJECTION_TYPES = ["Morfin", "Koksalt", "Naringsvatska" ]
-    INJECTION_LOCALIZATION = ["Hoger arm", "Vanster Arm"]    
+        for time in times:
+
+            df_vitals.loc[vitals_counter, :] = [
+                pid,
+                time,
+                "Puls",
+                round(75 * np.random.normal(1, .15))
+            ]
+            vitals_counter += 1
+
+            df_vitals.loc[vitals_counter, :] = [pid, time, "Kroppstemperatur",
+                                                round(37.2 + np.random.normal(0, .1), 1)]
+            vitals_counter += 1
+
+            df_vitals.loc[vitals_counter, :] = [
+                pid,
+                time,
+                "Blodtryck",
+                (BLOOD_PRESSURES[int(np.random.randint(0, len(BLOOD_PRESSURES)))])
+            ]
+            vitals_counter += 1
+
+            df_vitals.loc[vitals_counter, :] = [pid, time, "Andningsfrekvens",
+                                                round(19 * np.random.normal(1, .3), 1)]
+            vitals_counter += 1
+
+    # [Filling in df_injectios. Will need to add all available types,
+    # localizationsm and procedures.]
+
+    INJECTION_TYPES = ["Morfin", "Koksalt", "Naringsvatska"]
+    INJECTION_LOCALIZATION = ["Hoger arm", "Vanster Arm"]
     injection_counter = 0
-    for id in df_patients.id:
-        if np.random.uniform(0,1) < .4: #this changes number of patients with infarter
-            times = random_times(2,2*60)
-            df_injections.loc[injection_counter,:] = [id, times[0], times[1], random.choice(INJECTION_TYPES), random.choice([.1,.2,.3,.4]), random.choice(INJECTION_LOCALIZATION), "Seporerad" ]
+    for pid in df_patients.id:
+        if np.random.uniform(0, 1) < .4: #this changes number of patients with infarter
+            times = random_times(2, 2 * 60)
+            df_injections.loc[injection_counter, :] = [
+                pid,
+                times[0],
+                times[1],
+                random.choice(INJECTION_TYPES),
+                random.choice([.1, .2, .3, .4]),
+                random.choice(INJECTION_LOCALIZATION),
+                "Seporerad"
+            ]
             injection_counter += 1
-    
-    
-    """[Filling in df_event.
-        We still need to fill in all possible event categories and types. And they should probably be linked together, i assume this is used to specify icons and such.,
-        Entry and exit evennt are always entered.
-        All Patients have betwen 2 and 10 events.
-        ]
-    """
-    EVENT_CATEGORIES = ["Gubbe", "Doktor", "Pippett", "Ambulans", "Hus", "Medkit", "Heartbeat"]
-    EVENT_TYPES = ["Labbsvar Blodprov", "Labbsvar EKG", "Omvardnad", "Dosering"] 
+
+    # [Filling in df_event.
+    # We still need to fill in all possible event categories and types.
+    # And they should probably be linked together, i assume this is used to specify icons and such.,
+    # Entry and exit evennt are always entered.
+    # All Patients have betwen 2 and 10 events.]
+
+    EVENT_TYPES = ["Labbsvar Blodprov", "Labbsvar EKG", "Omvardnad", "Dosering"]
     EVENT_TYPES_SENT = ["Skickat Blodprov", "Skickat EKG", "Skickat Röntgen remiss"]
     EVENT_INLAGD = ["Gubbe", "Ambulans"]
 
-    def generate_type(isSentType):
-        stringToReturn = ""
-        if isSentType:
-            stringToReturn = random.choice(EVENT_TYPES_SENT)
+    def generate_type(is_sent_type):
+        string_to_return = ""
+        if is_sent_type:
+            string_to_return = random.choice(EVENT_TYPES_SENT)
         else:
-            stringToReturn = random.choice(EVENT_TYPES)
-        return stringToReturn
+            string_to_return = random.choice(EVENT_TYPES)
+        return string_to_return
 
-    # blodprov = pippett 
+    # blodprov = pippett
     # Gubbe = inlagd
     # Doktor = röntgen remiss, omvardnad
     # Ambulans =
     # Hus = lamnar
     # Medkit = dosering
     # Heartbeat = ekg
-    def generate_category(eventType):
-        stringToReturn = ""
-        if eventType == "Labbsvar Blodprov" or eventType == "Skickat Blodprov":
-            stringToReturn = "Pippett"
-        elif eventType == "Labbsvar EKG" or eventType == "Skickat EKG":
-            stringToReturn = "Heartbeat"
-        elif eventType == "Skickat Röntgen remiss" or eventType == "Omvardnad":
-            stringToReturn = "Doktor"
-        elif eventType == "Dosering":
-            stringToReturn = "Medkit"     
-        return stringToReturn
+    def generate_category(event_type):
+        string_to_return = ""
+        if event_type in ("Labbsvar Blodprov", "Skickat Blodprov"):
+            string_to_return = "Pippett"
+        elif event_type in ("Labbsvar EKG", "Skickat EKG"):
+            string_to_return = "Heartbeat"
+        elif event_type in ("Skickat Röntgen remiss", "Omvardnad"):
+            string_to_return = "Doktor"
+        elif event_type == "Dosering":
+            string_to_return = "Medkit"
+        return string_to_return
 
     total_event_counter = 0
-    for id in df_patients.id:
+    for pid in df_patients.id:
         #entry_time, exit_time = random_times(2,8*60)
-        
         MAX_EVENTS = 12
-        times = random_times(MAX_EVENTS,maxdiff_mins=3*60)
+        times = random_times(MAX_EVENTS, maxdiff_mins=3 * 60)
         timein = times[0]
-        timeout = datetime.time(times[-1].hour+1, times[-1].minute, times[-1].second)
-        times = [datetime.time(t.hour +1, t.minute, t.second) for t in times] # times between 1 and 3 hours
+        timeout = datetime.time(times[-1].hour + 1, times[-1].minute, times[-1].second)
+        # times between 1 and 3 hours
+        times = [datetime.time(t.hour + 1, t.minute, t.second) for t in times]
 
-        df_events.loc[total_event_counter,:] = [id, timein,random.choice(EVENT_INLAGD), "Patient Inlagd", False]
-        total_event_counter +=1
-        for i in range(int(random.randint(2,MAX_EVENTS-2))): #number of events per patient
-            if random.uniform(0,1) < 0.2:
-                isSent = True
-            else:
-                isSent = False
-            eventType = generate_type(isSent)
-            df_events.loc[total_event_counter,:] = [id, times[i], generate_category(eventType), eventType, isSent]
-            total_event_counter +=1
-        df_events.loc[total_event_counter,:] = [id, timeout,"Hus", "Patient Lamnar", False]
-        total_event_counter +=1
+        df_events.loc[total_event_counter, :] = [
+            pid,
+            timein,
+            random.choice(EVENT_INLAGD),
+            "Patient Inlagd",
+            False
+        ]
+        total_event_counter += 1
+        for i in range(int(random.randint(2, MAX_EVENTS - 2))): #number of events per patient
+            is_sent = random.uniform(0, 1) < 0.2
+            event_type = generate_type(is_sent)
+            df_events.loc[total_event_counter, :] = [
+                pid,
+                times[i],
+                generate_category(event_type),
+                event_type,
+                is_sent
+            ]
+            total_event_counter += 1
+        df_events.loc[total_event_counter, :] = [pid, timeout, "Hus", "Patient Lamnar", False]
+        total_event_counter += 1
 
-    
-
-    """  Generates values for the patients UMS """
+    # Generates values for the patients UMS
     ums_counter = 0
-    for id in df_patients.id:
-        sensLevel = random.randint(0,3)
-        medCondition = random.choice([True, False])
-        careDeviation = random.choice([True, False])
+    for pid in df_patients.id:
+        sens_level = random.randint(0, 3)
+        med_condition = random.choice([True, False])
+        care_deviation = random.choice([True, False])
         infection = random.choice([True, False])
-        noStructureInfo = random.choice([True, False])
-        df_ums.loc[ums_counter,:] = [id, sensLevel, medCondition, careDeviation, infection, noStructureInfo]
-        ums_counter +=1
+        no_structure_info = random.choice([True, False])
+        df_ums.loc[ums_counter, :] = [
+            pid,
+            sens_level,
+            med_condition,
+            care_deviation,
+            infection,
+            no_structure_info
+        ]
+        ums_counter += 1
 
-    
     print(df_patients.head(), "\n")
-    print(df_vitals.head(), "\n")       
+    print(df_vitals.head(), "\n")
     print(df_events, "\n")
     print(df_injections.head(), "\n")
-    
-    
-    
-    with open("mock_patient_data.csv", "w+") as f:
-        df_patients.to_csv(f, index=False)
-    
-    with open("mock_vitals.csv", "w+") as f:
-        df_vitals.to_csv(f, index=False)
-        
-    with open("mock_events.csv", "w+") as f:
-        df_events.to_csv(f, index=False)
-        
-    with open("mock_injections.csv", "w+") as f:
-        df_injections.to_csv(f, index=False)
 
-    with open("mock_ums.csv", "w+") as f:
-        df_ums.to_csv(f, index=False)
+    with open("mock_patient_data.csv", "w+") as file:
+        df_patients.to_csv(file, index=False)
 
-    
-    
-    
-    
-    return
-        
+    with open("mock_vitals.csv", "w+") as file:
+        df_vitals.to_csv(file, index=False)
+
+    with open("mock_events.csv", "w+") as file:
+        df_events.to_csv(file, index=False)
+
+    with open("mock_injections.csv", "w+") as file:
+        df_injections.to_csv(file, index=False)
+
+    with open("mock_ums.csv", "w+") as file:
+        df_ums.to_csv(file, index=False)
+
 if __name__ == '__main__':
     make_data()
     #print(generate_single_patient_vitals(1337,tot_num=100)["types"])
