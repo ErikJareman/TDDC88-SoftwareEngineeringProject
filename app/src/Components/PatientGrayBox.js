d7f9667f4d022ce452dd645cc8a45466018fd350
Gustav <gusch593@student.liu.se>
2021-11-24 09:20:57 +0100

    deleted gray box class

--- app/src/Components/PatientGrayBox.js
+++
@@ -1,8 +0,0 @@
-/* Gustav and John, trying to create a component that covers the gray area between*/
-
-import React from 'react'
-import './PatientGrayBox.css'
-
-<div id="grayBox" >
-    this is a gray box
-</div>
