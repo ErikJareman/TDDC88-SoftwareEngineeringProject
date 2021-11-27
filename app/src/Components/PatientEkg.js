19c21b26c7e360757abcb213221044d02784538c
Hugo Hallstensson <hugri003@student.liu.se>
2021-11-27 12:11:35 +0100

    added date and rounded time for EG

--- app/src/Components/PatientEkg.js
+++ app/src/Components/PatientEkg.js
@@ -9,11 +9,13 @@ import patientEkg from '../assets/patient_ekg.png'
 
 export default function PatientEkg (props) {
   const ekg = props.ekg[0]
+  var today = new Date()
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
   return (
     <>
       <h2>
         {ekg !== undefined
-          ? <><img src={patientEkg} ></img>  <h4> Datum: Date here {ekg.time} </h4></>
+          ? <><img src={patientEkg} ></img>  <h4> Datum: {date} Tid:  {ekg.time.substring(0, 8)} </h4></>
           /* <p>
             {'Datum: ' + ekg.time.getFullYear() + '-' + ekg.time.getMonth() + '-' + ekg.time.getDay()}
             </p>
